28ba646fbca72de9713da33c20ec0d5e3019a3c7
Smooth Operator <github@andy.disruptek.com>
2021-10-25 10:40:17 -0400

    allow errors in crossover

--- gread/crossover.nim
+++ gread/crossover.nim
@@ -10,14 +10,13 @@ proc subtreeCrossover*[T](a, b: Ast[T]): Ast[T] =
   audit b: echo b
   if a.len == 0:
     raise Defect.newException "xover input is empty"
-  elif a.len < 3:
-    result = b
   else:
     while true:
-      # 1 + rand ... ie. skip the root node
-      var (x, y) = (rand(1..a.high), rand(b.high))
+      var (x, y) = (rand(a.high), rand(b.high))
 
-      if not a.isFunctionSymbol(x) and not b.isFunctionSymbol(y):
+      # disabling this for now; i'd rather suffer the error
+      #if not a.isFunctionSymbol(x) and not b.isFunctionSymbol(y):
+      when true:
         # FIXME: optimize this delete/insert to a replace operation
         let dad = a.parentOf(x)   # parent of a subtree
         let d = a.delete(x)       # remove the old subtree
