b700ac0669ecf18b57ecf2cda0d72aecd8c0c10e
Smooth Operator <github@andy.disruptek.com>
2021-10-26 13:39:20 -0400

    break out aliasmethod

--- gread/manager.nim
+++ gread/manager.nim
@@ -1,7 +1,6 @@
 import std/options
 import std/random
 import std/hashes
-import std/deques
 
 import gread/population
 import gread/tableau
@@ -10,6 +9,7 @@ import gread/spec
 import gread/programs
 import gread/primitives
 import gread/data
+import gread/aliasmethod
 
 type
   Manager*[T] = object
@@ -20,20 +20,9 @@ type
     population: Population[T]
     operators: AliasMethod[Operator[T]]
 
-  AliasMethod*[T] = object
-    prob: seq[float]
-    alias: seq[int]
-    data: seq[T]
-
   Operator*[T] = proc(pop: var Population[T]): Option[Program[T]] {.nimcall.}
   Weight = float or float64
   OperatorWeight*[T] = tuple[operator: Operator[T]; weight: float64]
-
-proc newManager*[T](platform: T; tableau: Tableau; primitives: Primitives[T];
-                    operators: openArray[(Operator[T], float64)] = @[];
-                    fitness: Fitness[T] = nil): Manager[T] =
-  result = Manager(platform: platform, tableau: tableau, primitives: primitives)
-  initAliasMethod(result.operators, operators)
 
 proc tableau*(man: Manager): Tableau = man.tableau
 
@@ -47,59 +36,11 @@ proc `primitives=`*[T](man: var Manager[T]; primitives: Primitives[T]) =
 proc `population=`*[T](man: var Manager[T]; population: Population[T]) =
   man.population = population
 
-# vose alias method per https://en.wikipedia.org/wiki/Alias_method
-proc initAliasMethod[T](am: var AliasMethod; input: openArray[(Operator[T], Weight)]) =
-  var n = input.len
-  setLen(am.prob, n)
-  setLen(am.alias, n)
-  setLen(am.data, n)
-
-  # rescale the probabilities according to the quantity
-  var operators = newSeqOfCap[OperatorWeight[T]](n)
-  for (op, weight) in input.items:
-    operators.add (op, weight * n.float)
-
-  # sort the ops into deques of indices, according probability
-  var small, large: Deque[int]
-  for i, pair in operators.pairs:
-    am.data[i] = pair.operator
-    if pair.weight < 1.0:
-      small.addLast i
-    else:
-      large.addLast i
-
-  # consume and accumulate weights
-  while small.len > 0 and large.len > 0:
-    # consume
-    let (l, g) = (popFirst small, popFirst large)
-    am.prob[l] = operators[l].weight
-    am.alias[l] = g
-    # accumulate remainder
-    operators[g].weight += operators[l].weight-1.0
-    if operators[g].weight < 1.0:
-      small.addLast g
-    else:
-      large.addLast g
-
-  # consume the remaining
-  while large.len > 0:
-    let g = popFirst large
-    am.prob[g] = 1.0
-
-  while small.len > 0:
-    let l = popFirst small
-    am.prob[l] = 1.0
-
-proc len(am: AliasMethod): int = am.data.len
-
-proc choose[T](am: AliasMethod[T]): T =
-  ## weighted random choice from the list of operators
-  let i = rand(am.prob.len-1)
-  `[]`(am.data):
-    if rand(1.0) < am.prob[i]:
-      i
-    else:
-      am.alias[i]
+proc newManager*[T](platform: T; tableau: Tableau; primitives: Primitives[T];
+                    operators: openArray[(Operator[T], float64)] = @[];
+                    fitness: Fitness[T] = nil): Manager[T] =
+  result = Manager(platform: platform, tableau: tableau, primitives: primitives)
+  initAliasMethod(result.operators, operators)
 
 proc randomOperator*[T](man: Manager[T]): Operator[T] =
   if man.operators.len == 0:
