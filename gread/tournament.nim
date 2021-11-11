82f839b448054d20575537bd7bf3596f57506947
Smooth Operator <github@andy.disruptek.com>
2021-11-11 18:17:39 -0500

    add debugging

--- gread/tournament.nim
+++ gread/tournament.nim
@@ -1,3 +1,5 @@
+import std/math
+import std/packedsets
 import std/random
 import std/options
 import std/heapqueue
@@ -19,25 +21,36 @@ type
     index: int
     program: Program[T]
 
+const
+  debugging = false
+template debug(args: varargs[untyped]): untyped =
+  when debugging:
+    echo args
+
+import std/os
+template think(final: untyped): untyped =
+  when debugging:
+    echo final.program
+    sleep 10_000
+
 proc tournament*[T, V](evo: Evolver[T, V]; size: int;
                        order = Descending): Competitor[T] =
   ## find the fittest or least fit of a subset of the population
 
-  proc remover(competitors: var seq[Competitor[T]]; index: int) =
+  # FIXME: this code does not take parsimony into account
+
+  proc remover(competitors: var seq[Competitor[T]]; i: int) =
     ## used to update the population with a score change prior to removal
-    let c = competitors[index]
-    if c.program.isNil:
-      raise
+    let c = competitors[i]
     let s = evo.scoreFromCache(c.program)
     let score =
       if s.isSome:
         get s
       else:
         NaN
-    c.program.score = score
-    c.program.zombie = score.isValid
-    evo.population.scoreChanged(c.program, score, index = some c.index)
-    del(competitors, index)
+    debug "rm ", i, " was ", c.program.score, " now ", score
+    evo.population.scoreChanged(c.program, s, c.index)
+    del(competitors, i)
 
   if evo.population.len < 1:
     raise ValueError.newException:
@@ -48,12 +61,19 @@ proc tournament*[T, V](evo: Evolver[T, V]; size: int;
 
   # select quantity N random programs; they must be unique.
   var victims: seq[Competitor[T]]     # programs we'll test
+  var seen: PackedSet[int]            # de-dupe victims
+  var caches: float                   # average size of caches for victims
   while victims.len < size:
     var (i, p) = randomMember evo.population
     if p.isNil:
       raise
-    victims.add (valid: not p.zombie, score: Score NaN,
-                 len: p.len, index: i, program: p)
+    if not seen.containsOrIncl i:
+      caches += p.cacheSize.float
+      victims.add (valid: not p.zombie, score: Score NaN,
+                   len: p.len, index: i, program: p)
+  caches = ceil(caches / victims.len.float)
+  #debug "victims:"
+  #debug victims
 
   block byebye:
 
@@ -63,13 +83,19 @@ proc tournament*[T, V](evo: Evolver[T, V]; size: int;
       samples.add i
     shuffle samples
 
+    debug ""
+    debug ""
+    debug "sample order:"
+    debug samples
+
     var data: seq[SymbolSet[T, V]]       # datapoints we've tested
-    while samples.len > 0 and victims.len > 1:
+    while samples.len > 0 and victims.len > 1 and data.len < caches.int:
       # pick a novel datapoint we have not tested previously
       data.add evo.dataset[pop samples]
 
       # test all victims against the datapoint
       var i = 0
+      debug "scoring against ", data[^1]
       while i <= victims.high and victims.len > 1:
         let p = victims[i].program
         if p.isNil:
@@ -79,14 +105,28 @@ proc tournament*[T, V](evo: Evolver[T, V]; size: int;
           block:
             # re-score the program against all datapoints tested to date
             let s = evo.score(data, p)
+            #let s = evo.scoreFromCache(p)  # use the best number we have!
             if s.isNone:
               # weird, but we must treat this as a very bad failure
+              debug "victim ", i, " failed score against all data"
               break
             else:
+              # update the competitor and move to the next victim
+              debug "victim ", i, " was ", victims[i].score, " now ", get s
               victims[i].score = get s
               victims[i].valid = victims[i].score.isValid
-            inc i
-            continue
+              debug "victim ", i, " ", victims[i].program
+              inc i
+              continue
+        else:
+          debug "victim ", i, " failed score against ", data[^1]
+          debug "victim ", i, " ", victims[i].program
+          if s.isSome:
+            debug "victim ", i, " scored ", get s
+          victims[i].score = NaN
+          victims[i].valid = false
+
+        debug "victim ", i, " is invalid"
 
         # the program failed a fitone or a fitmany; toss it
         if order == Ascending:
@@ -97,13 +137,18 @@ proc tournament*[T, V](evo: Evolver[T, V]; size: int;
           # just remove it from the tournament and continue on
           remover(victims, i)
 
-      # if programs remain,
-      if victims.len > 1:
-        # sort the remainder, and
-        sort(victims, order)
-        # remove any losers
-        while victims[^1].score != victims[0].score:
-          remover(victims, victims.high)
+      if data.len >= caches.int:
+        # if programs remain,
+        if victims.len > 1:
+          # sort the remainder, and
+          sort(victims, order)
+          debug "removing victims worse than ", victims[0].score
+
+          # remove any losers
+          while victims[^1].score != victims[0].score:
+            remover(victims, victims.high)
+
+      debug "loop done at ", i, " with data len ", data.len, " order ", order
 
     # the result will be the greatest winner
     result = victims[0]
@@ -111,3 +156,7 @@ proc tournament*[T, V](evo: Evolver[T, V]; size: int;
   # remove the remaining victims to inform the population
   while victims.len > 0:
     remover(victims, victims.high)
+
+  debug order, " program cache ", result.program.cacheSize, " average is ", Score caches
+  debug "final result: ", result
+  think result
