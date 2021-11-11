61b94174ddd736ccf1102b4d42e135b92229d5e3
Smooth Operator <github@andy.disruptek.com>
2021-11-11 13:32:49 -0500

    add toggle for requiring valid programs

--- gread/tableau.nim
+++ gread/tableau.nim
@@ -7,9 +7,11 @@ type
     tournamentSize*: int
     useParsimony*: bool
     sharingRate*: float
+    requireValid*: bool
 
 const
   defaultTableau* =
     Tableau(seedPopulation: 500, maxPopulation: 500,
             maxGenerations: 10_000_000, seedProgramSize: 5,
+            requireValid: true,
             sharingRate: 2.0, tournamentSize: 6, useParsimony: on)
