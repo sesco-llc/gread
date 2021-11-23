79c690bcc5f293225c0c57d77f92fda5236ed6ad
Smooth Operator <github@andy.disruptek.com>
2021-11-23 11:29:27 -0500

    add equality for terminals

--- gread/ast.nim
+++ gread/ast.nim
@@ -148,6 +148,9 @@ proc hash*(a: Terminal): Hash =
   h = h !& hash(a.kind)
   result = !$h
 
+proc `==`*(a, b: Terminal): bool =
+  a.kind == b.kind and a.hash == b.hash
+
 func high*(a: Ast): int {.inline.} = a.nodes.high
 func low*(a: Ast): int {.inline.} = a.nodes.low
 
