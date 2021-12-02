f7400cdc55354f2d3d3b4c1c3358db130ddc0edd
Smooth Operator <github@andy.disruptek.com>
2021-12-02 18:53:04 -0500

    fix omission of tail in crossover

--- gread/crossover.nim
+++ gread/crossover.nim
@@ -27,20 +27,17 @@ proc geCrossover*[T](gram: Grammar[T];
   ## perform GE crossover between two parents to form a new child
   if a.len == 0 or b.len == 0:
     raise ShortGenome.newException "received empty input genome"
-  var g: Genome
-  if a.len == 1:
-    g = a & b
-  elif b.len == 1:
-    g = b & a
-  else:
-
-    # ensure the first crossover point occurs in mutual coding space
-    let x = rand(0..min(a.high, b.high))
-    # the second point can exceed the length of the shorter genome
-    let y = rand(x..b.high)
-    g = a[0..<x] & b[x..y]
+  # ensure the first crossover point occurs in mutual coding space
+  let x = rand(0..min(a.high, b.high))
+  # the second point can exceed the length of the shorter genome
+  let y = rand((x+1)..b.len)
+  var g = a[0..<x]
+  g.add b[x..<y]
+  if y < a.high:
+    g.add a[y..a.high]
   try:
     let (pc, ast) = gram.πGE(g)                       # map the new genome
-    result = some: (ast: ast, genome: g[0..<pc.int])  # throw away the tail
+    result = some (ast: ast, genome: g[0..<pc.int])
   except ShortGenome:
-    discard
+    #echo "short crossover; ", a.len, " and ", b.len, " and ", g.len
+    raise
