5b4243a0597f4e809ae2d402d584457c61719ff9
Smooth Operator <github@andy.disruptek.com>
2021-12-02 18:59:05 -0500

    update example

--- examples/lls.nim
+++ examples/lls.nim
@@ -1,7 +1,6 @@
 import std/options
 import std/os
 import std/osproc
-import std/strformat
 import std/hashes
 import std/random
 import std/math
@@ -9,7 +8,6 @@ import std/math
 import gread
 import gread/fennel except variance
 
-import pkg/balls
 import pkg/cps
 import pkg/lunacy
 import pkg/loony
@@ -18,22 +16,22 @@ import pkg/adix/lptabz
 const
   goodEnough = 0.1      # termination condition
   statFrequency = 10000  # report after this many generations
+  llsGrammar = """
+    <start>        ::= <numexpr>
+    <numexpr>      ::= ( <numbop> <numexpr> <numexpr> )
+    <numexpr>      ::= <value>
+    <numbop>       ::= "+" | "*" | "-" | "/"
+    <value>        ::= "1.0" | "0.5" | "0.1" | "2.0"
+    <value>        ::= "x"
+  """
 
-var prims = newPrimitives[Fennel]()
-prims.functions = @[
-  fun("+", arity=2), fun("-", arity=2),
-  fun("*", arity=2), fun("/", arity=2),
-]
-prims.constants = @[term 1.0, term 0.1]
+var gram: Grammar[Fennel]
+initGrammar(gram, llsGrammar)
 
 # you can adjust these weights to change mutation rates
 let operators = {
-  randomCrossover[Fennel, LuaValue]:    1.0,
-  pointPromotion[Fennel, LuaValue]:    15.0,
-  removeOneLeaf[Fennel, LuaValue]:     30.0,
-  appendOneLeaf[Fennel, LuaValue]:      2.0,
-  pointMutation[Fennel, LuaValue]:      4.0,
-  subtreeCrossover[Fennel, LuaValue]:  90.0,
+  geCrossover[Fennel, LuaValue]:  50.0,
+  geMutation[Fennel, LuaValue]:   25.0,
 }
 
 const
@@ -41,7 +39,6 @@ const
   data = @[(1,6), (2,5), (3,7), (4,10)]
 
 # preparing the data for use in the fitness()
-prims.inputs.add [sym"x"]
 var dataset: seq[Locals]
 for (x, y) in data.items:
   dataset.add:
@@ -73,17 +70,18 @@ proc fitmany(fnl: Fennel; data: openArray[(Locals, Score)];
     else:
       results.add s
   if results.len > 0:
-    result = some Score -ss(results)
+    result =
+      some:
+        Score -ss(results)
 
 when isMainModule:
-  import std/sequtils
   import gread/cluster
 
   randomize()
 
   # the main loop monitors inventions
   proc main(work: Work; inputs, outputs: LoonyQueue[FProg]) =
-    let fnl = newFennel(work.primitives)
+    let fnl = newFennel()
     var best: Program[Fennel]
     while true:
       let p = pop inputs
@@ -102,16 +100,18 @@ when isMainModule:
   # now setup the workers with their unique populations, etc.
   var tab = defaultTableau
   tab.useParsimony = false  # our scoring isn't -1.0..1.0
-  tab.seedPopulation = 100
-  tab.maxPopulation = 100
+  tab.seedPopulation = 3_000
+  tab.seedProgramSize = 100
+  tab.maxPopulation = 3_000
   tab.tournamentSize = 10
-  tab.sharingRate = 6.0
+  tab.sharingRate = 2.0
 
   # each worker gets a Work object as input to its thread
   let clump = newCluster[Fennel, LuaValue]()
   var args = clump.initWork()
-  initWork(args, tab, prims, operators = operators, dataset = dataset,
-           fitone = fitone, fitmany = fitmany, stats = statFrequency)
+  initWork(args, tab, grammar = gram, operators = operators,
+           dataset = dataset, fitone = fitone, fitmany = fitmany,
+           stats = statFrequency)
 
   for core in 0..<countProcessors():
     clump.boot(whelp worker(args), args.core)
