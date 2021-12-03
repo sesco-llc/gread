af8f691f55c8545f79604e75fb9a66ba50c5b656
Smooth Operator <github@andy.disruptek.com>
2021-12-02 19:29:38 -0500

    rm ast crossover

--- gread/crossover.nim
+++ gread/crossover.nim
@@ -6,22 +6,6 @@ import gread/ast
 import gread/grammar
 import gread/genotype
 
-proc subtreeCrossover*[T](a, b: Ast[T]): Ast[T] =
-  audit a: echo a
-  audit b: echo b
-  if a.len == 0:
-    raise Defect.newException "xover input is empty"
-  else:
-    var (x, y) = (rand(a.high), rand(b.high))
-
-    # disabling this for now; i'd rather suffer the error
-    #if not a.isFunctionSymbol(x) and not b.isFunctionSymbol(y):
-    # FIXME: optimize this delete/insert to a replace operation
-    let c = b.subtree(y)      # the subtree at index y
-    audit c: echo "chunk in xover: ", c
-    result = a.replace(x, c)   # replace node(s) at x with new subtree
-  audit result: echo "xover result: ", result
-
 proc geCrossover*[T](gram: Grammar[T];
                      a, b: Genome): Option[tuple[ast: Ast[T]; genome: Genome]] =
   ## perform GE crossover between two parents to form a new child
