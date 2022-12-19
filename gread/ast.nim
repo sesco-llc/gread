20db3b99fe2860698aaa27792802eb85120271e7
Smooth Operator <github@andy.disruptek.com>
2022-12-19 11:40:49 -0500

    rm useless error

--- gread/ast.nim
+++ gread/ast.nim
@@ -188,7 +188,7 @@ func numberOfChildren*[T](n: AstNode[T]): int =
   else:
     0
 
-func sizeOfSubtree*[T](a: Ast[T]; index = 0): int =
+proc sizeOfSubtree*[T](a: Ast[T]; index = 0): int =
   ## the size, in nodes, of the tree at the given index
   mixin isParent
   audit a: debug "sizeof subtree: ", $a
