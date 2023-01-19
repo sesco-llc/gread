451d046ac72f35fde91358cce9b975862d50914a
Smooth Operator <github@andy.disruptek.com>
2023-01-19 08:03:39 -0500

    example grammars are more interesting

--- examples/preamble.nim
+++ examples/preamble.nim
@@ -1,28 +1,29 @@
 
 const
   greadSeed {.intdefine.} = 0
-  goodEnough = -0.10000     # termination condition
+  goodEnough = -0.40000     # termination condition
   llsMany {.intdefine.} = 100_000
   manyGenerations = llsMany
-  statFrequency = 10_000
+  statFrequency = 5_000
   llsGrammar = """
     <start>        ::= <numexpr>
-    <numexpr>      ::= ( <numbop> <numexpr> <numexpr> )
+    <varargs>      ::= <numexpr> | <numexpr> <varargs>
+    <numexpr>      ::= ( <numbop> <numexpr> <varargs> )
     <numexpr>      ::= <value>
     <numexpr>      ::= <value>
     <numbop>       ::= "+" | "*" | "-" | "/"
-    <value>        ::= "1.0" | "0.5" | "0.1" | "2.0"
+    <value>        ::= "1.0" | "2.0" | "0.5" | "0.1"
     <value>        ::= "x"
   """
-  tourney = 0.03
+  tourney = 0.04
 
 # define the parameters for the evolvers
 var tab = defaultTableau
 tab -= {UseParsimony}
 tab -= {RequireValid, EqualWeight}
-tab.seedProgramSize = 300
-tab.seedPopulation = 300
+tab.seedProgramSize = 500
+tab.seedPopulation = 100
 tab.maxPopulation = tab.seedPopulation
 tab.tournamentSize = int(tourney * tab.maxPopulation.float)
-tab.sharingRate = 0.004
+tab.sharingRate = 0.030
 tab.maxGenerations = manyGenerations
